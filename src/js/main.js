505dac274d625be46c91babec14ab29a23c68e87
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2021-11-25 14:29:34 +0100

    changed objects to maps

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,60 @@
 "use strict";
 
 const haushaltsbuch = {
-    gesamtbilanz: {
-        einnahmen: 0,
-        ausgaben: 0,
-        bilanz: 0
-    },
+    gesamtbilanz: new Map(),
 
     eintraege: [],
 
     eintrag_erfassen() {
-        this.eintraege.push(
-            {
-            titel: prompt("Titel:"),
-            typ: prompt("Typ (Einnahme oder Ausgabe):"),
-            betrag: parseInt(prompt("Betrag (in Cent):")),
-            datum: prompt("Datum (jjjj-mm-tt")
-        }
-
-        );
+        let neuer_eintrag = new Map();
+        neuer_eintrag.set("titel", prompt("Titel:"));
+        neuer_eintrag.set("typ", prompt("Typ (Einnahme oder Ausgabe):"));
+        neuer_eintrag.set("betrag", parseInt(prompt("Betrag (in Cent):")));
+        neuer_eintrag.set("datum", prompt("Datum (jjjj-mm-tt"));
+        this.eintraege.push(neuer_eintrag);
    
+    },
+
+    eintraege_sortieren() {
+        this.eintraege.sort(function(eintrag_a, eintrag_b) {
+            if(eintrag_a.get("datum") > eintrag_b.get("datum")) {
+                return -1;
+            } else if(eintrag_a.get("datum")  < eintrag_b.get("datum")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
     },
     
     eintraege_ausgeben() {
         console.clear();
         this.eintraege.forEach(function(eintrag) {
-            console.log(`Titel: ${eintrag.titel}\n`
-            + `Typ: ${eintrag.typ}\n`
-            + `Betrag: ${eintrag.betrag} ct\n`
-            + `Datum: ${eintrag.datum}`
+            console.log(`Titel: ${eintrag.get("titel")}\n`
+            + `Typ: ${eintrag.get("typ")}\n`
+            + `Betrag: ${eintrag.get("betrag")} ct\n`
+            + `Datum: ${eintrag.get("datum")}`
             );
         });
     },
 
     gesamtbilanz_erstellen() {
-        let neue_gesamtbilanz = {
-            einnahmen:  0,
-            ausgaben:  0,
-            bilanz: 0
-        };
+        let neue_gesamtbilanz = new Map();
+        neue_gesamtbilanz.set("einnahmen", 0);
+        neue_gesamtbilanz.set("ausgaben", 0);     
+        neue_gesamtbilanz.set("bilanz", 0);
         this.eintraege.forEach(function(eintrag) {
-            switch(eintrag.typ) {
+            switch(eintrag.get("typ")) {
                 case "Einnahme":
-                    neue_gesamtbilanz.einnahmen += eintrag.betrag;
-                    neue_gesamtbilanz.bilanz += eintrag.betrag;
+                    neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
+                    neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") + eintrag.get("betrag"));
                     break;
                 case "Ausgabe":
-                    neue_gesamtbilanz.ausgaben += eintrag.betrag;
-                    neue_gesamtbilanz.bilanz -= eintrag.betrag;
+                    neue_gesamtbilanz.set("ausgaben", neue_gesamtbilanz.get("ausgaben") + eintrag.get("betrag"));
+                    neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") - eintrag.get("betrag"));
                     break;
                 default: 
-                console.log(`Der Typ "${eintrag.typ}" ist nicht bekannt!`);
+                console.log(`Der Typ "${eintrag.get("typ")}" ist nicht bekannt!`);
                     break;
             }
         });
@@ -58,24 +62,12 @@ const haushaltsbuch = {
         
     },
 
-    eintraege_sortieren() {
-        this.eintraege.sort(function(eintrag_a, eintrag_b) {
-            if(eintrag_a.datum > eintrag_b.datum) {
-                return -1;
-            } else if(eintrag_a.datum < eintrag_b.datum) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-    },
-
 
     gesamtbilanz_ausgeben() {
-    console.log(`Einnahmen: ${this.gesamtbilanz.einnahmen} ct\n`
-        + `Ausgaben: ${this.gesamtbilanz.ausgaben} ct\n`
-        + `Bilanz: ${this.gesamtbilanz.bilanz} ct\n`
-        + `Bilanz ist positiv ${this.gesamtbilanz.bilanz >= 0}`);    
+    console.log(`Einnahmen: ${this.gesamtbilanz.get("einnahmen")} ct\n`
+        + `Ausgaben: ${this.gesamtbilanz.get("ausgaben")} ct\n`
+        + `Bilanz: ${this.gesamtbilanz.get("bilanz")} ct\n`
+        + `Bilanz ist positiv ${this.gesamtbilanz.get("bilanz") >= 0}`);    
     },
 
 
