c923dc8dd60f2b82c2cdf52514a9dbe5780d8883
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2021-12-22 23:26:34 +0100

    Die Methode formulardaten_validieren()

--- src/js/eingabeformular.js
+++ src/js/eingabeformular.js
@@ -28,6 +28,25 @@ const eingabeformular = {
         }
     },
 
+    formulardaten_validieren(formulardaten) {
+
+        let fehler = [];
+        if (formulardaten.titel === "") {
+            fehler.push("Titel");
+        }
+        if(formulardaten.typ === undefined || formulardaten.typ.match(/^(?:einnahme|ausgabe)$/) == null) {
+            fehler.push("Typ");
+        }
+        if(isNaN(formulardaten.betrag)) {
+            fehler.push("Betrag");
+        }
+        if(formulardaten.datum === null) {
+            fehler.push("Datum");
+        }
+        return fehler;
+
+    },
+
     absenden_event_hinzufuegen(eingabeformular) {
         eingabeformular.querySelector("#eingabeformular").addEventListener("submit", e => {
             e.preventDefault();
@@ -35,6 +54,8 @@ const eingabeformular = {
             console.log(e);
             let formulardaten = this.formulardaten_verarbeiten(this.formulardaten_holen(e));
             console.log(formulardaten);
+            let formular_fehler = this.formulardaten_validieren(formulardaten);
+            console.log(formular_fehler);
             // Formulardaten validieren
             // wenn die Formulardaten valide sind
                 // Eintrag zum Haushaltsbuch hinzufügen
@@ -63,7 +84,7 @@ const eingabeformular = {
                 <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size="10" title="Titel des Eintrags">
                 <input type="radio" id="einnahme" name="typ" value="einnahme" form="eingabeformular" title="Typ des Eintrags">
                 <label for="einnahme" title="Typ des Eintrags">Einnahme</label>
-                <input type="radio" id="ausgabe" name="typ" value="ausgabe" form="eingabeformular" title="Typ des Eintrags" checked>
+                <input type="radio" id="ausgabe" name="typ" value="ausgabe" form="eingabeformular" title="Typ des Eintrags">
                 <label for="ausgabe" title="Typ des Eintrags">Ausgabe</label>
             </div>
         </div>
