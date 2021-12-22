795473dbb6679ee109bd4bcbff8e069aebf133df
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2021-12-22 18:30:04 +0100

    Das Submit-Event abfangen und weiter planen

--- src/js/eingabeformular.js
+++ src/js/eingabeformular.js
@@ -2,6 +2,25 @@
 
 const eingabeformular = {
 
+    absenden_event_hinzufuegen(eingabeformular) {
+        eingabeformular.querySelector("#eingabeformular").addEventListener("submit", e => {
+            e.preventDefault();
+            // Formulardaten holen
+            // Formulardaten verarbeiten
+            // Formulardaten validieren
+            // wenn die Formulardaten valide sind
+                // Eintrag zum Haushaltsbuch hinzufügen
+                   // wenn bereits Fehlermeldung angezeigt wird
+                    // Fehldermeldung entfernen
+                // Formular zurücksetzen
+                // Datum auf das heutigen Tag setzen
+            // wenn die Formulardaten NICHT valide sind
+                // wenn bereits Fehlermeldung angezeigt wird
+                    // Fehldermeldung entfernen
+                // Fehlermeldung im Eingabeformular-Container anzeigen
+        });
+    },
+
     html_generieren() {
         
         let eingabeformular = document.createElement("section");
@@ -32,11 +51,14 @@ const eingabeformular = {
             <button class="standard" type="submit" form="eingabeformular">Hinzufügen</button>
         </div>`;
 
+        this.absenden_event_hinzufuegen(eingabeformular);
+
         return eingabeformular;
     },
 
     anzeigen() {
         document.querySelector("#navigationsleiste").insertAdjacentElement("afterend", this.html_generieren());
+        // Datum auf das heutigen Tag setzen
     }
 
 };
