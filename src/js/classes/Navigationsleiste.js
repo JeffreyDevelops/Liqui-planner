1579d370713671e7dcd59308834a055bb6e74649
Jeffrey <88632169+JeffreyEj@users.noreply.github.com>
2022-01-06 00:18:15 +0100

    Navigationsleiste

--- src/js/classes/Navigationsleiste.js
+++ src/js/classes/Navigationsleiste.js
@@ -1,10 +1,39 @@
 "use strict";
 
-{/* <nav id="navigationsleiste">
+/*
+<nav id="navigationsleiste">
 <a href="index.html"><span id="markenname">Liqui-Planner</span></a>
-</nav> */}
+</nav> 
+*/
 
 class Navigationsleiste {
 
+    constructor() {
+        this._html = this._html_generieren();
+    }
+
+    _html_generieren() {
+        let navigationsleiste = document.createElement("nav");
+        navigationsleiste.setAttribute("id", "navigationsleiste");
+
+        let anker = document.createElement("a");
+        anker.setAttribute("href", "#");
+
+        let span = document.createElement("span");
+        span.setAttribute("id", "markenname");
+        span.textContent = "Liqui-Planner";
+        anker.insertAdjacentElement("afterbegin", span);
+
+        navigationsleiste.insertAdjacentElement("afterbegin", anker);
+
+        return navigationsleiste;
+    }
+
+    anzeigen() {
+        let body = document.querySelector("body");
+        if (body !== null) {
+            body.insertAdjacentElement("afterbegin", this._html);
+        }
+    }
     
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+    let navigationsleiste = new Navigationsleiste();
+    navigationsleiste.anzeigen();
+
     let eingabeformular = new Eingabeformular();
     eingabeformular.anzeigen();
 
